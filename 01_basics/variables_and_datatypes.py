0185f34d131413fbcd1497d2d5a5854cd64da1a8
sormilabagui <sumontabagui189@gmail.com>
2025-08-16 22:35:59 +0530

    added first program on variables and datatypes

---
+++ 01_basics/variables_and_datatypes.py
@@ -0,0 +1,7 @@
+age = 22
+name = "casiana"
+is_student = True
+
+print("Name :",name, " - type :",type(name))
+print("age :",age, " - type :",type(age))
+print("is student :",is_student, " - type :",type(is_student))
