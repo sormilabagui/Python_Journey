c9984cabcfcc16437e9dc1f714964b360a3bdaa9
sormilabagui <sumontabagui189@gmail.com>
2025-08-17 23:53:03 +0530

    added operators program in basics today

---
+++ 01_basics/operators.py
@@ -0,0 +1,24 @@
+#trying different types of operators in python
+
+a=10
+b=3
+
+#arithmetic operators
+print("Add :",a," +",b," = ",a+b)
+print("Sub :",a," -",b," = ",a-b)
+print("Mul :",a," *",b," = ",a*b)
+print("Div :",a," /",b," = ",a/b)
+print("Modulo :",a," //",b," = ",a//b)
+print("exponent :",a," **",b," = ",a**b)
+
+#comparision operators
+print("is a equal to b? :", a==b)
+print("is a not equal to b? :", a!=b)
+print("is a greater to b? :", a>b)
+print("is a lesser to b? :", a<b)
+
+# logical Operators
+x, y = True, False
+print("x and y:", x and y)
+print("x or y:", x or y)
+print("not x:", not x)
