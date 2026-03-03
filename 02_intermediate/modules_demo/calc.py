7bb408bd456cdfc8ff02c677aef158c9d774cdc2
sormilabagui <sumontabagui189@gmail.com>
2026-03-03 22:45:01 +0530

    Add custom module example with calculator package demo

--- 02_intermediate/modules_demo/calc.py
+++ 02_intermediate/modules_demo/calc.py
@@ -1,1 +1,15 @@
 #this file acts as a custom module
+
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    if b == 0:
+        return "cannot divide by zero..."
+    return a/b
---
+++ 02_intermediate/modules_demo/main.py
@@ -0,0 +1,8 @@
+# importing out custom module
+
+import calc
+
+print("Addition:", calc.add(10, 5))
+print("Subtraction:", calc.sub(10, 5))
+print("Multiplication:", calc.mul(10, 5))
+print("Division:", calc.div(10, 5))
